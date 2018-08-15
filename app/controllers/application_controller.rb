6f39cbfdd316c08ce67d38c1d39b7ac2c8a06bd8
Daniel Lirio Reis <daniellirioreis@gmail.com>
2018-08-15 20:17:21 -0300

    Controlar numero de posts
    Não mostrar posts em branco

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,32 +4,44 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :definir_posts
-  
   def definir_posts
+    @count_posts_sale = 0
   	@post1 = Post.favorite(true).for_type_post(TypePost::FOR_SALE)[0] 
   	 if @post1.nil? 
   	 	@post1 = Post.new
+     else
+      @count_posts_sale =  @count_posts_sale + 1
   	 end	
   	@post2 = Post.favorite(true).for_type_post(TypePost::FOR_SALE)[1]
   	if @post2.nil? 
   		@post2 = Post.new
+    else
+       @count_posts_sale =  @count_posts_sale + 1
   	end	  	
   	@post3 = Post.favorite(true).for_type_post(TypePost::FOR_SALE)[2]
   	if @post3.nil?
   		@post3 = Post.new
+    else
+        @count_posts_sale =  @count_posts_sale + 1
   	end
-
+     @count_posts_rent = 0
     @post4 = Post.favorite(true).for_type_post(TypePost::FOR_RENT)[0] 
      if @post4.nil? 
       @post4 = Post.new
+     else
+       @count_posts_rent =  @count_posts_rent + 1 
      end  
     @post5 = Post.favorite(true).for_type_post(TypePost::FOR_RENT)[1]
     if @post5.nil? 
       @post5 = Post.new
+    else
+       @count_posts_rent =  @count_posts_rent + 1       
     end     
     @post6 = Post.favorite(true).for_type_post(TypePost::FOR_RENT)[2]
     if @post6.nil?
       @post6 = Post.new
+    else
+       @count_posts_rent =  @count_posts_rent + 1       
     end
   end
  	
